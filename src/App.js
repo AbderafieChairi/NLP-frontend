2f97178a5e7e3e4e0f403dcd6afdcd430c96d1ef
Abderafie <chairi.abderafie@student.emi.ac.ma>
2023-05-02 17:18:51 +0000

    first try

--- src/App.js
+++ src/App.js
@@ -1,56 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
-import Flow from './Drag/Drag';
-import { useIntent } from './contexts/IntentContext';
-import Detail from './Detail';
-import {db} from './config/firebase'
-import { collection, getDocs } from 'firebase/firestore';
-import { useApp } from './contexts/AppContext';
-import Chat from './components/Chat/Chat';
-import {UnControlled as CodeMirror} from 'react-codemirror2'
-import Editor from './Editor';
-import Entity from './components/entities/Entity';
+
 function App() {
-  const {showdetail,addNode} = useIntent();
-  const {showChat} = useApp(); 
-  const [code, setCode] = useState("") 
-  React.useEffect(()=>{
-    console.log("new feature")
-  },[])
+  const [inp, setInp] = useState("do you have a phone ?")
+  const [varName, setVarName] = useState("")
   return (
-    <div className="App"
-    onClick={(e)=>{
-      e.preventDefault();
-    }}
-    >
-      <Flow />
-      {showdetail.showdetail&&<Detail/>}
-      <Dash/>
-      {showChat&&<Chat/>}
-      {/* <Entity/> */}
-
-
+    <div className="App">
+      <input value={inp} onChange={(e) => setInp(e.target.value)} />
+      <div>variable name</div>
+      <input value={varName} onChange={(e) => setVarName(e.target.value)} />
     </div>
   );
 }
 //some changes
 export default App;
-
-
-export function Dash(){
-  const {addNode,menuPosition,initNodes} = useIntent()
-  const {setShowChat} =  useApp()
-  return (
-    <div className='intent-dash' style={{top:menuPosition.y,left:menuPosition.x}}>
-      <button onClick={(e)=>{
-        e.preventDefault();
-        // addNode(e.clientX,e.clientY)
-      }}>add Intent</button>
-      <button onClick={()=>initNodes()}>clear</button>
-      <button onClick={()=>setShowChat(e=>!e)}>chat</button>
-      <button >entities</button>
-    </div>
-  )
-}
 
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import IntentProvider from './contexts/IntentContext';
-import AppProvider from './contexts/AppContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <AppProvider>
-      <IntentProvider>
         <App/>
-      </IntentProvider>
-    </AppProvider>
 );
 
